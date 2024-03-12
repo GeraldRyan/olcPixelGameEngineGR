0db8e90125d8902491f2c8755bb7f4523339f671
GeraldRyan <gerald.ryan40@yahoo.com>
2024-03-11 22:00:42 -0500

    in progress not a good stopping point, code is different with this header for linux

--- src/engine.cpp
+++ src/engine.cpp
@@ -1,7 +1,23 @@
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 
-class olcEngine3D :  public olc::PixelGameEngine
+using namespace std;
+struct vec3d
+{
+    float x, y, z;
+};
+
+struct triangle
+{
+    vec3d p[3];
+};
+
+struct mesh
+{
+    vector<triangle> tris;
+};
+
+class olcEngine3D : public olc::PixelGameEngine
 {
 public:
     olcEngine3D()
@@ -9,15 +25,40 @@ public:
         sAppName = "3D Demo";
     }
 
+private:
+    mesh meshCube;
+
 public:
     bool OnUserCreate() override
     {
+        meshCube.tris = {
+//south
+{0.0f, 0.0f, 0.0f,      0.0f, 1.0f, 0.0f,      1.0f, 1.0f, 0.0f},
+{0.0f, 0.0f, 0.0f,      1.0f, 1.0f, 0.0f,      1.0f, 0.0f, 0.0f},
+//east
+{1.0f, 0.0f, 0.0f,      1.0f, 1.0f, 0.0f,      1.0f, 1.0f, 1.0f},
+{1.0f, 0.0f, 0.0f,      1.0f, 1.0f, 1.0f,      1.0f, 0.0f, 1.0f},
+//north
+{1.0f, 0.0f, 1.0f,      1.0f, 1.0f, 1.0f,      0.0f, 1.0f, 1.0f},
+{1.0f, 0.0f, 1.0f,      0.0f, 1.0f, 1.0f,      0.0f, 0.0f, 1.0f},
+//west
+{0.0f, 0.0f, 1.0f,      0.0f, 1.0f, 1.0f,      0.0f, 1.0f, 0.0f},
+{0.0f, 0.0f, 1.0f,      0.0f, 1.0f, 0.0f,      0.0f, 0.0f, 0.0f},
+//top
+{0.0f, 1.0f, 0.0f,      0.0f, 1.0f, 1.0f,      1.0f, 1.0f, 1.0f},
+{0.0f, 1.0f, 0.0f,      1.0f, 1.0f, 1.0f,      1.0f, 1.0f, 0.0f},
+//bottom
+{1.0f, 0.0f, 1.0f,      0.0f, 0.0f, 1.0f,      0.0f, 0.0f, 0.0f},
+{1.0f, 0.0f, 1.0f,      0.0f, 0.0f, 0.0f,      1.0f, 0.0f, 0.0f},
+
+        };
         return true;
     }
 
     bool OnUserUpdate(float fElapsedTime) override
     {
-        
+// fill(0,0,ScreenWidth(), ScreenHeight(), PIXEL_SOLID, FG_BLACK);
+FillRect(0,0,ScreenWidth(), ScreenHeight(), olc::BLACK);
         return true;
     }
 };
